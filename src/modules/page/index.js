67f4efdf903d05bb3b8c9f98feeb82030dad7e3f
clay53 <clayton.hickey@yahoo.com>
2020-04-04 14:38:49 -0400

    Finished contact page

--- src/modules/page/index.js
+++ src/modules/page/index.js
@@ -3,7 +3,7 @@ const nav = require('./nav');
 const footer = require('./footer');
 const googleAnalyics = require('./googleAnalytics');
 
-module.exports = (main, onlyContent=false) => {
+module.exports = (main, seo={}, onlyContent=false) => {
     var content = `<html><head><link rel="stylesheet" type="text/css" href="/static/styles.css">${config.mailchimpCode}${googleAnalyics}</head><body>${nav}<div id="page-container">${main}</div>${footer}</body>`;
     if (onlyContent) {
         return content;
--- src/w/news/posts/post.js
+++ src/w/news/posts/post.js
@@ -4,7 +4,7 @@ const postsList = require('./list');
 module.exports = (req, res) => {
     var fileName = req.params.postFileName;
     if (Object.keys(postsList).includes(fileName)) {
-        res.send(page(postsList[fileName].content, true));
+        res.send(page(postsList[fileName].content, {}, true));
     } else {
         console.error("Requested post " + fileName + " not found.");
     }
